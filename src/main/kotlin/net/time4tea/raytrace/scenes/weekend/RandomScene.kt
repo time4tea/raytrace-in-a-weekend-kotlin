3c473322684c7c0dabe00877ceb9cc90b1d2288f
James Richardson <james@time4tea.net>
2019-01-27 22:28:16 +0000

    random scene

---
+++ src/main/kotlin/net/time4tea/raytrace/scenes/weekend/RandomScene.kt
@@ -0,0 +1,46 @@
+package net.time4tea.raytrace.scenes.weekend
+
+import net.time4tea.raytrace.*
+import kotlin.random.Random
+
+class RandomScene : Scene {
+
+    override fun lookfrom() = Vec3(13f, 2f, 3f)
+    override fun lookat() = Vec3(0f, 0f, 0f)
+
+    override fun scene(): Hitable {
+
+        val list = mutableListOf<Hitable>()
+
+        for (a in -11..11) {
+            for (b in -11..11) {
+                val choose_mat = Random.nextFloat()
+
+                val centre = Vec3(a + 0.9f * Random.nextFloat(), 0.2f, b + 0.9f * Random.nextFloat())
+                if ((centre - Vec3(4.0f, 0.2f, 0.0f)).length() > 0.9) {
+
+                    val material = when {
+                        choose_mat < 0.8 -> Metal(
+                            Vec3(
+                                0.5f * (1f + Random.nextFloat()),
+                                0.5f * (1f + Random.nextFloat()),
+                                0.5f * (1f + Random.nextFloat())
+                            ), 0.5f * Random.nextFloat()
+                        )
+                        choose_mat < 0.95 -> Lambertian(
+                            ConstantTexture(Vec3(Random.nextFloat(), Random.nextFloat(), Random.nextFloat()))
+                        )
+                        else -> Dielectric(1.5f)
+                    }
+
+                    val item = when (Random.nextInt(2)) {
+                        0 -> Sphere(centre, 0.2f, material)
+                        else -> Box(centre - Vec3(0.1, 0.1, 0.1), centre + Vec3(0.1, 0.1, 0.1), material)
+                    }
+                    list.add(item)
+                }
+            }
+        }
+        return BVH(list)
+    }
+}
--- src/main/kotlin/net/time4tea/raytrace/scenes/weekend/WeekendFinal.kt
+++ src/main/kotlin/net/time4tea/raytrace/scenes/weekend/WeekendFinal.kt
@@ -2,10 +2,9 @@ package net.time4tea.raytrace.scenes.weekend
 
 import net.time4tea.raytrace.*
 import java.io.File
-import kotlin.random.Random
 
-interface Scene{
-    fun scene() : Hitable
+interface Scene {
+    fun scene(): Hitable
     fun lookat(): Vec3
     fun lookfrom(): Vec3
 }
@@ -15,42 +14,6 @@ class WeekendFinal : Scene {
     override fun lookfrom() = Vec3(13f, 2f, 3f)
     override fun lookat() = Vec3(0f, 0f, 0f)
 
-    private fun random_scene(): List<Hitable> {
-
-        val list = mutableListOf<Hitable>()
-
-        for (a in -11..11) {
-            for (b in -11..11) {
-                val choose_mat = Random.nextFloat()
-
-                val centre = Vec3(a + 0.9f * Random.nextFloat(), 0.2f, b + 0.9f * Random.nextFloat())
-                if ((centre - Vec3(4.0f, 0.2f, 0.0f)).length() > 0.9) {
-
-                    val material = when {
-                        choose_mat < 0.8 -> Metal(
-                            Vec3(
-                                0.5f * (1f + Random.nextFloat()),
-                                0.5f * (1f + Random.nextFloat()),
-                                0.5f * (1f + Random.nextFloat())
-                            ), 0.5f * Random.nextFloat()
-                        )
-                        choose_mat < 0.95 -> Lambertian(
-                            ConstantTexture(Vec3(Random.nextFloat(), Random.nextFloat(), Random.nextFloat()))
-                        )
-                        else -> Dielectric(1.5f)
-                    }
-
-                    val item = when (Random.nextInt(2)) {
-                        0 -> Sphere(centre, 0.2f, material)
-                        else -> Box(centre - Vec3(0.1, 0.1, 0.1), centre + Vec3(0.1, 0.1, 0.1), material)
-                    }
-                    list.add(item)
-                }
-            }
-        }
-        return list
-    }
-
     override fun scene(): Hitable {
         val green_white = CheckerTexture(
             ConstantTexture(Vec3(0.2, 0.3, 0.1)),
@@ -59,24 +22,23 @@ class WeekendFinal : Scene {
 
         val earth = ImageTexture(File("src/main/resources/earth.jpg"))
 
-        return BVH(
+        return HitableList(
             listOf(
-                Sphere(
-                    Vec3(0, -1000, 0), 1000f, Lambertian(green_white)
-                ),
-                Sphere(Vec3(0, 1, 0), 1.0f, Dielectric(1.5)),
-                Sphere(
-                    Vec3(-4, 1, 0), 1.0f, Lambertian(
-                        earth
+                BVH(
+                    listOf(
+                        Sphere(
+                            Vec3(0, -1000, 0), 1000f, Lambertian(green_white)
+                        ),
+                        Sphere(Vec3(0, 1, 0), 1.0f, Dielectric(1.5)),
+                        Sphere(
+                            Vec3(-4, 1, 0), 1.0f, Lambertian(
+                                earth
+                            )
+                        ),
+                        Sphere(Vec3(4, 1, 0), 1.0f, Metal(Vec3(0.7, 0.6, 0.5), 0.0f))
                     )
-                ),
-                Sphere(Vec3(4, 1, 0), 1.0f, Metal(Vec3(0.7, 0.6, 0.5), 0.0f))
-//        ConstantMedium(
-//            Sphere(Vec3(360, 150, 145), 70.0f, Dielectric(1.5)),
-//            0.2f,
-//            ConstantTexture(Vec3(0.2, 0.4, 0.9))
-//        )
-            ) + random_scene()
+                ), RandomScene().scene()
+            )
         )
     }
 
