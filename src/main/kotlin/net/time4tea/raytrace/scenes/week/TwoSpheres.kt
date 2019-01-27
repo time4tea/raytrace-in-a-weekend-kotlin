3653a95dceaae67434e30ae5d1ab257dba880b83
James Richardson <james@time4tea.net>
2019-01-27 22:24:02 +0000

    add two spheres scene

---
+++ src/main/kotlin/net/time4tea/raytrace/scenes/week/TwoSpheres.kt
@@ -0,0 +1,27 @@
+package net.time4tea.raytrace.scenes.week
+
+import net.time4tea.raytrace.*
+import net.time4tea.raytrace.scenes.weekend.Scene
+
+class TwoSpheres : Scene {
+    override fun scene(): Hitable {
+        val checker = CheckerTexture(
+            ConstantTexture(Vec3(0.2, 0.3, 0.1)), ConstantTexture(Vec3(0.9, 0.9, 0.9))
+        );
+
+        return BVH(
+            listOf(
+                Sphere(Vec3(0, -10, 0), 10f, Lambertian(checker)),
+                Sphere(Vec3(0, 10, 0), 10f, Lambertian(checker))
+            )
+        )
+    }
+
+    override fun lookat(): Vec3 {
+        return Vec3.ZERO
+    }
+
+    override fun lookfrom(): Vec3 {
+        return Vec3(50,50,50)
+    }
+}
