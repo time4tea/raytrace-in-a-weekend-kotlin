48848e133b8c70320440bfc232bf040a9024b109
James Richardson <james@time4tea.net>
2019-10-24 17:46:59 +0100

    remove unused imports

--- src/main/kotlin/net/time4tea/raytrace/Scene.kt
+++ src/main/kotlin/net/time4tea/raytrace/Scene.kt
@@ -1,8 +1,5 @@
 package net.time4tea.raytrace
 
-import net.time4tea.raytrace.Hitable
-import net.time4tea.raytrace.Vec3
-
 interface Scene {
     fun scene(): Hitable
     fun constantLight(): (Vec3) -> Vec3 = { Vec3.ZERO }
