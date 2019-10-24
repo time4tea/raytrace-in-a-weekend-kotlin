2acb79c5d396c0ffed94f48bc5cff82de0159b42
James Richardson <james@time4tea.net>
2019-10-24 16:53:56 +0100

    fix typo bug - still not quite right though...

--- src/main/kotlin/net/time4tea/raytrace/ConstantMedium.kt
+++ src/main/kotlin/net/time4tea/raytrace/ConstantMedium.kt
@@ -1,7 +1,6 @@
 package net.time4tea.raytrace
 
-import kotlin.math.E
-import kotlin.math.log
+import kotlin.math.ln
 import kotlin.random.Random
 
 class ConstantMedium(
@@ -13,13 +12,13 @@ class ConstantMedium(
     private val phase_function = Isotropic(texture)
 
     override fun hit(ray: Ray, min: Float, max: Float): Hit? {
-        return boundary.hit(ray, -Float.MAX_VALUE, Float.MAX_VALUE)?.let { hit1 ->
+        return boundary.hit(ray, Float.MIN_VALUE, Float.MAX_VALUE)?.let { hit1 ->
             boundary.hit(ray, hit1.t + 0.0001f, Float.MAX_VALUE)?.let { hit2 ->
                 if (hit1.t >= hit2.t) {
                     null
                 } else {
-                    val distance_inside_boundary = (hit1.t - hit2.t) * ray.direction().length()
-                    val hit_distance = -(1.0f / density) * log(Random.nextFloat(), E.toFloat())
+                    val distance_inside_boundary = (hit2.t - hit1.t) * ray.direction().length()
+                    val hit_distance = -(1.0f / density) * ln(Random.nextFloat())
                     if (hit_distance < distance_inside_boundary) {
                         val t = hit1.t + hit_distance / ray.direction().length()
                         Hit(
