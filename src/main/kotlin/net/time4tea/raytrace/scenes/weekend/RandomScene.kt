977d7e4a606b8e6ac1180841cb84e5d43e609437
James Richardson <james+teal@time4tea.net>
2021-01-03 19:18:39 +0000

    random scene is reproducible

--- src/main/kotlin/net/time4tea/raytrace/scenes/weekend/RandomScene.kt
+++ src/main/kotlin/net/time4tea/raytrace/scenes/weekend/RandomScene.kt
@@ -5,6 +5,8 @@ import kotlin.random.Random
 
 class RandomScene : Scene {
 
+    val random = Random(1234)
+
     override fun lookfrom() = Vec3(13f, 2f, 3f)
     override fun lookat() = Vec3(0f, 0f, 0f)
 
@@ -14,26 +16,26 @@ class RandomScene : Scene {
 
         for (a in -11..11) {
             for (b in -11..11) {
-                val choose_mat = Random.nextFloat()
+                val choose_mat = random.nextFloat()
 
-                val centre = Vec3(a + 0.9f * Random.nextFloat(), 0.2f, b + 0.9f * Random.nextFloat())
+                val centre = Vec3(a + 0.9f * random.nextFloat(), 0.2f, b + 0.9f * random.nextFloat())
                 if ((centre - Vec3(4.0f, 0.2f, 0.0f)).length() > 0.9) {
 
                     val material = when {
                         choose_mat < 0.8 -> Metal(
                             Colour(
-                                0.5f * (1f + Random.nextFloat()),
-                                0.5f * (1f + Random.nextFloat()),
-                                0.5f * (1f + Random.nextFloat())
-                            ), 0.5f * Random.nextFloat()
+                                0.5f * (1f + random.nextFloat()),
+                                0.5f * (1f + random.nextFloat()),
+                                0.5f * (1f + random.nextFloat())
+                            ), 0.5f * random.nextFloat()
                         )
                         choose_mat < 0.95 -> Lambertian(
-                            ConstantTexture(Colour(Random.nextFloat(), Random.nextFloat(), Random.nextFloat()))
+                            ConstantTexture(Colour(random.nextFloat(), random.nextFloat(), random.nextFloat()))
                         )
                         else -> Dielectric(1.5f)
                     }
 
-                    val item = when (Random.nextInt(2)) {
+                    val item = when (random.nextInt(2)) {
                         0 -> Sphere(centre, 0.2f, material)
                         else -> Box(centre - Vec3(0.1, 0.1, 0.1), centre + Vec3(0.1, 0.1, 0.1), material)
                     }
